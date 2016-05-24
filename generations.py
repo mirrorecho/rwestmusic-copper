cc64368b1c83ca7a80c319b6c3a604d4a0bf1e84
Randall West <rwest@IT102AML.local>
2016-05-24 01:11:07 -0500

    copper pitch rows

---
+++ generations.py
@@ -0,0 +1,24 @@
+
+
+from abjad import *
+
+# 3 pitch rows
+# create a routine to get the 3rd note
+# create a routine to arrange pitches
+
+row = [9, 7, 6, 2, -1, 1, 2]
+
+notes = [Note(p, (1,8)) for p in row]
+notes2 = [Note(p-7, (1,8)) for p in row]
+
+st1 = Staff()
+st1.extend(notes)
+
+st2 = Staff()
+st2.extend(notes2)
+
+sc = Score()
+sc.append(st1)
+sc.append(st2)
+
+show(sc)
