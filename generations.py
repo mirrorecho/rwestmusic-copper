ef5641eff2f6902a890ecd6896fda4aad5cf63ae
Randall West <rwest@IT102AML.local>
2016-05-26 00:54:04 -0500

    started integrating calliope

--- generations.py
+++ generations.py
@@ -12,6 +12,8 @@ row = [9, 7, 6, 2, -1, 1, 2]
 
 notes = [Note(p, (1,8)) for p in row]
 notes2 = [Note(p-7, (1,8)) for p in row]
+row3 = [row[(i+3) % len(row) ]+7 for i in range(len(row)) ]
+notes3 = [Note(p, (1,8)) for p in row3]
 
 st1 = Staff()
 st1.extend(notes)
@@ -19,8 +21,13 @@ st1.extend(notes)
 st2 = Staff()
 st2.extend(notes2)
 
+st3 = Staff()
+st3.extend(notes3)
+
 sc = Score()
+sc.append(st3)
 sc.append(st1)
 sc.append(st2)
+
 
 show(sc)
